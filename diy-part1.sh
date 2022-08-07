0e85a68d8659858375af954eff5278dd5d0386db
SK-20211125WMLN\Gakki <1825063964@qq.com>
2022-08-07 23:22:56 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,18 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+set FORCE_UNSAFE_CONFIGURE=1
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
-echo 'src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
+echo 'src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb.git' >>feeds.conf.default
+echo 'src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssr.git' >>feeds.conf.default
+echo "src-git openwisp_config https://github.com/openwisp/openwisp-config.git^1.0.0" >>feeds.conf.default
+echo "src-git openwisp_monitoring https://github.com/openwisp/openwrt-openwisp-monitoring.git" >>feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+echo "CONFIG_PACKAGE_netjson-monitoring=y" >> .config
+echo "CONFIG_PACKAGE_openwisp-monitoring=y" >> .config
+make defconfig
+make tools/install
+make toolchain/install
+make package/openwisp-monitoring/compile
